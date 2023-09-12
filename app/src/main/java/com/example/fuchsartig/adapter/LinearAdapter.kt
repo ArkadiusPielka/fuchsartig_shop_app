553996ca2470741f64d76cf1041336b45338583a
arkadiuspielka <akki1980@hotmail.de>
2023-09-12 12:28:02 +0200

    LinearAdapter hinzugefügt

---
+++ app/src/main/java/com/example/fuchsartig/adapter/LinearAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.fuchsartig.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.fuchsartig.data.model.Product
+import com.example.fuchsartig.databinding.ListItemLinearBinding
+
+class LinearAdapter(private val dataSet: List<Product>) : RecyclerView.Adapter<LinearAdapter.LinearViewHolder>(){
+
+
+    class LinearViewHolder(val binding: ListItemLinearBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LinearViewHolder {
+        val binding = ListItemLinearBinding.inflate(LayoutInflater.from(parent.context), parent,false)
+        return LinearViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    override fun onBindViewHolder(holder: LinearViewHolder, position: Int) {
+        val product = dataSet[position]
+        val binding = holder.binding
+        val imgUri = product.img1.toUri().buildUpon().scheme("https").build()
+
+        binding.imgProduct.load(imgUri)
+        binding.tvPrice.text = product.price
+        binding.tvDescription.text = product.descript
+        binding.tvTitle.text = product.title
+    }
+}
