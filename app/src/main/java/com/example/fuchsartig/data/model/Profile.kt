6d08360ecbdc0635465bd5c2150694da37e6febe
arkadiuspielka <akki1980@hotmail.de>
2023-09-26 13:07:52 +0200

    Model Profile erweitert

--- app/src/main/java/com/example/fuchsartig/data/model/Profile.kt
+++ app/src/main/java/com/example/fuchsartig/data/model/Profile.kt
@@ -1,9 +1,13 @@
 package com.example.fuchsartig.data.model
 
 data class Profile(
+    val gender: String = "",
     val firstName: String = "",
     val lastName: String = "",
-    val profilImg: String = "",
-
-
+    val profileImg: String = "",
+    val street: String = "",
+    val hausNr: String = "",
+    val plz: String = "",
+    val country: String = "",
+    val land: String = "",
 )
